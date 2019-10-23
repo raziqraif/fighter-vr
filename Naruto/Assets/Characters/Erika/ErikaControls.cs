18d283fff28adf6c0bd42e7a8f183e5788473475
Raziq Raif <42981908+raziqraif@users.noreply.github.com>
2019-10-23 15:48:52 -0400

    Add follow and attack mechanism for enemy.

---
+++ Naruto/Assets/Characters/Erika/ErikaControls.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ErikaControls : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject player;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (anim.GetBool("isDead"))
+        {
+            return;
+        }
+
+        transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), Vector3.up);
+
+
+        if ((Vector3.Distance(transform.position, player.transform.position) > 20)  && (!agent.pathPending) && (!agent.hasPath)) 
+        {
+            //Debug.Log("Set distance");
+            anim.SetBool("isShootRange", false);
+            anim.SetBool("isRunRange", true);
+            agent.SetDestination(player.transform.position);
+        }
+
+
+        if (!agent.velocity.Equals(Vector3.zero))
+        {
+            if (!anim.GetBool("isRunRange"))
+            {
+                
+                //Debug.Log("Bool running");
+                anim.SetBool("isShootRange", false);
+                anim.SetBool("isRunRange", true);
+            }
+        }
+
+        if (!agent.pathPending)
+        {
+            if (Vector3.Distance(agent.destination, player.transform.position) > 10f)
+            {
+                /*Debug.Log(agent.destination);
+                Debug.Log(player.transform.position);
+                Debug.Log(Vector3.SqrMagnitude(agent.destination - player.transform.position));
+                */
+                agent.ResetPath();
+                agent.SetDestination(player.transform.position);
+            }
+            if (agent.remainingDistance <= agent.stoppingDistance + 15)
+            {
+                //Debug.Log("Remaining distance");
+                if ((!agent.hasPath) || (agent.velocity.sqrMagnitude == 0f)
+                    || (agent.nextPosition == agent.transform.position))
+                {
+                    agent.ResetPath();
+                    anim.SetBool("isRunRange", false);
+                    anim.SetBool("isShootRange", true);
+                }
+            }
+        }
+    }
+}
--- Naruto/Assets/Characters/Kachujin/KachujinControls.cs
+++ Naruto/Assets/Characters/Kachujin/KachujinControls.cs
@@ -1,18 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class KachujinControls : MonoBehaviour
 {
+    NavMeshAgent agent;
+    GameObject player;
+    Animator anim;
+    string[] attackList = new string[4]{ "jumpKicking", "kicking", "punching", "roundhouseKicking" };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (anim.GetBool("dying"))
+        {
+            return;
+        }
+
+        transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), Vector3.up);
+
+
+        if ((Vector3.Distance(transform.position, player.transform.position) > 5f) && (!agent.pathPending) && (!agent.hasPath))
+        {
+            Debug.Log("Set distance");
+            setAnimationParameter("running");
+            agent.SetDestination(player.transform.position);
+        }
+
+
+        if (!agent.velocity.Equals(Vector3.zero))
+        {
+            if (!anim.GetBool("running"))
+            {
+                Debug.Log("Bool running");
+                setAnimationParameter("running");
+            }
+        }
+
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance + 2f)
+            {
+                Debug.Log("Kachujin: Remaining distance");
+                if ((!agent.hasPath) || (agent.velocity.sqrMagnitude == 0f)
+                    || (agent.nextPosition == agent.transform.position))
+                {
+                    agent.ResetPath();
+                    setAnimationParameter(attackList[new System.Random().Next(0, 3)]);
+                }
+            }
+            Debug.Log("Kachujin" + anim.GetBool("running"));
+             
+       }
+    }
+
+    void setAnimationParameter(string str)
+    {
+        anim.SetBool("running", false);
+        anim.SetBool("jumpKicking", false);
+        anim.SetBool("kicking", false);
+        anim.SetBool("punching", false);
+        anim.SetBool("roundhouseKicking", false);
+        anim.SetBool("dying", false);
+
+        anim.SetBool(str, true);
+
     }
 }
